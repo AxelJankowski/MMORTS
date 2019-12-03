1f4945ac4c0e20ead78cd063fe09a14decc479f2
Axel <axeljankowskii@gmail.com>
2019-12-03 17:17:02 +0100

    starting project of browser game

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    $maintenance = true;
+    if ($maintenance == true) {
+        echo "The site is under construction.";
+    } elseif ($maintenance == false) {
+        echo "All done.";
+    }
+?>
