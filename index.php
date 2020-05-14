f5879d545354f9fb20de6f4065689a5f99aa8bc8
Axel <axeljankowskii@gmail.com>
2020-05-14 19:07:25 +0200

    Screenshot.

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
     session_start();
     
     require_once("backend/system/includes.php");
-
     if ($maintenance == true) {
         //display maintenance message
         echo "The site is under construction.";
